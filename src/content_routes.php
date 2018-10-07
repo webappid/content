6a3a32212c731b0171dc3e8b0f33f71d5b7fe9de
Dyan Galih <dyan.galih@gmail.com>
2018-10-08 01:09:19 +0700

    fix file route

--- src/content_routes.php
+++ src/content_routes.php
@@ -4,11 +4,8 @@
     Route for file
 */
 Route::group(['prefix' => 'file'], function () {
-	Route::get('/', function () {
-		return view('sample.upload');
-	});
 	$routeCode = "file";
-	Route::get('{name}', 'Tools\FileController@index')->name($routeCode);
-	Route::get('{name}/{size}', 'Tools\FileController@show')->name($routeCode.'.resize');
-	Route::post('upload/{path}', 'Tools\FileController@create')->name($routeCode.'.upload');
+	Route::get('{name}', 'WebAppId\Content\Controllers\FileController@index')->name($routeCode);
+	Route::get('{name}/{size}', 'WebAppId\Content\Controllers\FileController@show')->name($routeCode.'.resize');
+	Route::post('upload/{path}', 'WebAppId\Content\Controllers\FileController@create')->name($routeCode.'.upload');
 });
