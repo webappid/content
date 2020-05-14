5866e3fd3cbecc23004c0d11a01f0d916672b996
Dyan Galih <dyan.galih@gmail.com>
2020-05-14 22:31:29 +0700

    Fix File Route

--- src/content_routes.php
+++ src/content_routes.php
@@ -26,7 +26,7 @@ if (isset($_SERVER["PHP_SELF"])) {
 
 Route::group(['prefix' => 'file'], function () {
     $routeCode = "file";
-    Route::get('{name}', 'WebAppId\Content\Controllers\FileController@index')->name($routeCode);
-    Route::get('{name}/{size}', 'WebAppId\Content\Controllers\FileController@show')->name($routeCode . '.resize');
-    Route::post('upload/{path}', 'WebAppId\Content\Controllers\FileController@create')->name($routeCode . '.upload');
+    Route::get('{name}', WebAppId\Content\Controllers\FileOriController::class)->name($routeCode);
+    Route::get('{name}/{size}', WebAppId\Content\Controllers\FileShowController::class)->name($routeCode . '.resize');
+    Route::post('upload/{path}', WebAppId\Content\Controllers\FileStoreController::class)->name($routeCode . '.upload');
 });
